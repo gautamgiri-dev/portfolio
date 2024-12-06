b558ec775e9f22fbd380eaa93d8c841246d4e804
Gautam Giri <personal.gautamgiri@gmail.com>
2024-12-06 05:45:23 +0000

    fixed apos error

--- src/app/under-development/page.tsx
+++ src/app/under-development/page.tsx
@@ -1,8 +1,8 @@
 export default function UnderDevelopment() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      I'm trying hard to keep up the pace but it seems this page is still under
-      development. Please check back later
+      I&apos;m trying hard to keep up the pace but it seems this page is still
+      under development. Please check back later
     </div>
   );
 }
